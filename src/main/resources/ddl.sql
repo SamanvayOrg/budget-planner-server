e59a1111fe0b5a81e01d795dbaa22626c08fdbe7
Sachin Kadam <Sachinkadamsk01@gmail.com>
2022-06-16 19:17:29 +0530

    sql changes

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -3,13 +3,19 @@ create table state
     id   serial primary key,
     name text
 );
+create table city_class
+(
+    id   serial primary key,
+    name text not null
+);
 
 create table municipality
 (
     id         serial primary key,
     name       varchar(255),
     population bigint,
-    state_id   int references state (id)
+    state_id   int references state (id),
+    class_id int references city_class
 );
 
 create table budget
@@ -80,14 +86,17 @@ create table login_user
 
 create table budget_line
 (
-    id               serial primary key,
-    budget_id        int not null references budget (id),
-    function_id      int not null references function (id),
-    detailed_head_id int not null references detailed_head (id),
-    planned_amount   numeric(15, 2),
-    revised_amount   numeric(15, 2),
-    actual_amount    numeric(15, 2),
-    display_order    numeric(7, 2)
+    id                          serial primary key,
+    name                        text,
+    budget_id                   int not null references budget (id),
+    function_id                 int not null references function (id),
+    detailed_head_id            int not null references detailed_head (id),
+    budgeted_amount             numeric(15, 2),
+    revised_amount              numeric(15, 2),
+    actual_amount               numeric(15, 2),
+    display_order               numeric(7, 2),
+    currentYear8MonthsActuals   numeric(15, 2),
+    currentYear4MonthsProbables numeric(15, 2)
 );
 
 alter table budget_line
@@ -96,13 +105,9 @@ alter table budget_line
 create table sample_budget_line
 (
     id               serial primary key,
+    name             text,
     state_id         integer not null references state (id),
     function_id      int     not null references function (id),
     detailed_head_id int     not null references detailed_head (id),
     display_order    numeric(7, 2)
-);
-create table city_class
-(
-    id   serial primary key,
-    name text not null
 );
--- src/main/resources/sample_data.sql
+++ src/main/resources/sample_data.sql
@@ -1,7 +1,11 @@
 insert into state(id, name) values (1, 'Maharashtra');
 
-insert into municipality (id, name, population, state_id) VALUES (1, 'Sinnar', '30000', 1);
-insert into municipality (id, name, population, state_id) VALUES (2, 'Wai', '50000', 1);
+insert into city_class(name)values ('Municipal council');
+insert into city_class(name)values ('Municipal corporation');
+insert into city_class(name)values ('Nagar panchayat');
+
+insert into municipality (id, name, population, state_id,class_id) VALUES (1, 'Sinnar', '30000', 1,1);
+insert into municipality (id, name, population, state_id,class_id) VALUES (2, 'Wai', '50000', 1,1);
 
 insert into budget(id, financial_year, municipality_id) VALUES (1, 2020, 1);
 insert into budget(id, financial_year, municipality_id) VALUES (2, 2021, 1);
@@ -15,55 +19,55 @@ insert into budget(id, financial_year, municipality_id) VALUES (8, 2023, 2);
 
 select * from detailed_head;
 
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (1, 1, 1, 1021, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (2, 1, 1, 1022, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (3, 1, 2, 1023, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (4, 1, 2, 1024, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (5, 1, 3, 1025, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (6, 1, 1, 1026, 1001.00, 1000.00, 900);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (1, 1, 1, 1021, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (2, 1, 1, 1022, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (3, 1, 2, 1023, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (4, 1, 2, 1024, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (5, 1, 3, 1025, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (6, 1, 1, 1026, 1001.00, 1000.00, 900,200,700);
 
 
 
 
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (7, 2, 1,  1021, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (8, 2, 1,  1022, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (9, 2, 2,  1023, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (10, 2, 2, 1024, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (11, 2, 3, 1025, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (12, 2, 1, 1026, 1001.00, 1000.00, 900);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (7, 2, 1,  1021, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (8, 2, 1,  1022, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (9, 2, 2,  1023, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (10, 2, 2, 1024, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (11, 2, 3, 1025, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (12, 2, 1, 1026, 1001.00, 1000.00, 900,200,700);
 
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (13, 3, 1, 1021, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (14, 3, 1, 1022, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (15, 3, 2, 1023, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (16, 3, 2, 1024, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (17, 3, 3, 1025, 1001.00, 1000.00, 900);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (18, 3, 1, 1026, 1001.00, 1000.00, 900);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (13, 3, 1, 1021, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (14, 3, 1, 1022, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (15, 3, 2, 1023, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (16, 3, 2, 1024, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (17, 3, 3, 1025, 1001.00, 1000.00, 900,200,700);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (18, 3, 1, 1026, 1001.00, 1000.00, 900,200,700);
 
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (19, 4, 1, 1021, 1001.00, 0, 0);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (20, 4, 1, 1022, 1001.00, 0, 0);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (21, 4, 2, 1023, 1001.00, 0, 0);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (22, 4, 2, 1024, 1001.00, 0, 0);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (23, 4, 3, 1025, 1001.00, 0, 0);
-insert into budget_line (id, budget_id, function_id, detailed_head_id, planned_amount, revised_amount, actual_amount) VALUES    (24, 4, 1, 1026, 1001.00, 0, 0);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (19, 4, 1, 1021, 1001.00, 0, 0,300,100);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (20, 4, 1, 1022, 1001.00, 0, 0,300,100);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (21, 4, 2, 1023, 1001.00, 0, 0,300,100);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (22, 4, 2, 1024, 1001.00, 0, 0,300,100);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (23, 4, 3, 1025, 1001.00, 0, 0,300,100);
+insert into budget_line (id, budget_id, function_id, detailed_head_id, budgeted_amount, revised_amount, actual_amount,currentYear8MonthsActuals,currentYear4MonthsProbables) VALUES    (24, 4, 1, 1026, 1001.00, 0, 0,300,100);
 
 insert into sample_budget_line
-(state_id, function_id, detailed_head_id, display_order)
-    (select 1, function_id, detailed_head_id, display_order
+(name,state_id, function_id, detailed_head_id, display_order)
+    (select name,1, function_id, detailed_head_id, display_order
      from budget_line
      where budget_id = 1);
 update budget_line set display_order=id;
 select * from budget;
 delete from budget_line where budget_id = 4;
 
-insert into sample_budget_line(id, state_id, function_id, detailed_head_id, display_order)values (1, 1, (select id from function where full_code = '910'), (select id from detailed_head where full_code = '1110'), 1);
-insert into sample_budget_line(id, state_id, function_id, detailed_head_id, display_order)values (2, 1, (select id from function where full_code = '910'), (select id from detailed_head where full_code = '1111'), 2);
-insert into sample_budget_line(id, state_id, function_id, detailed_head_id, display_order)values (3, 1, (select id from function where full_code = '920'), (select id from detailed_head where full_code = '1120'), 3);
-insert into sample_budget_line(id, state_id, function_id, detailed_head_id, display_order)values (4, 1, (select id from function where full_code = '920'), (select id from detailed_head where full_code = '1121'), 4);
-insert into sample_budget_line(id, state_id, function_id, detailed_head_id, display_order)values (5, 1, (select id from function where full_code = '310'), (select id from detailed_head where full_code = '2510'), 5);
-insert into sample_budget_line(id, state_id, function_id, detailed_head_id, display_order)values (6, 1, (select id from function where full_code = '314'), (select id from detailed_head where full_code = '2520'), 6);
-insert into sample_budget_line(id, state_id, function_id, detailed_head_id, display_order)values (7, 1, (select id from function where full_code = '210'), (select id from detailed_head where full_code = '3221'), 7);
-insert into sample_budget_line(id, state_id, function_id, detailed_head_id, display_order)values (8, 1, (select id from function where full_code = '315'), (select id from detailed_head where full_code = '4234'), 8);
-insert into sample_budget_line(id, state_id, function_id, detailed_head_id, display_order)values (9, 1, (select id from function where full_code = '740'), (select id from detailed_head where full_code = '3230'), 9);
+insert into sample_budget_line(id,name, state_id, function_id, detailed_head_id, display_order)values (1,(select name from function where full_code='910'),1, (select id from function where full_code = '910'), (select id from detailed_head where full_code = '1110'), 1);
+insert into sample_budget_line(id,name, state_id, function_id, detailed_head_id, display_order)values (2,(select name from function where full_code='910'),1, (select id from function where full_code = '910'), (select id from detailed_head where full_code = '1111'), 2);
+insert into sample_budget_line(id,name, state_id, function_id, detailed_head_id, display_order)values (3,(select name from function where full_code='920'),1, (select id from function where full_code = '920'), (select id from detailed_head where full_code = '1120'), 3);
+insert into sample_budget_line(id,name, state_id, function_id, detailed_head_id, display_order)values (4,(select name from function where full_code='920'),1, (select id from function where full_code = '920'), (select id from detailed_head where full_code = '1121'), 4);
+insert into sample_budget_line(id,name, state_id, function_id, detailed_head_id, display_order)values (5,(select name from function where full_code='310'),1, (select id from function where full_code = '310'), (select id from detailed_head where full_code = '2510'), 5);
+insert into sample_budget_line(id,name, state_id, function_id, detailed_head_id, display_order)values (6,(select name from function where full_code='314'),1, (select id from function where full_code = '314'), (select id from detailed_head where full_code = '2520'), 6);
+insert into sample_budget_line(id,name, state_id, function_id, detailed_head_id, display_order)values (7,(select name from function where full_code='210'),1, (select id from function where full_code = '210'), (select id from detailed_head where full_code = '3221'), 7);
+insert into sample_budget_line(id,name, state_id, function_id, detailed_head_id, display_order)values (8,(select name from function where full_code='315'),1, (select id from function where full_code = '315'), (select id from detailed_head where full_code = '4234'), 8);
+insert into sample_budget_line(id,name, state_id, function_id, detailed_head_id, display_order)values (9,(select name from function where full_code='740'),1, (select id from function where full_code = '740'), (select id from detailed_head where full_code = '3230'), 9);
 
 
 
